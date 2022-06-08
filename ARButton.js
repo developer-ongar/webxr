f10b3e8f83b4ee4ccde475388d4ab562043d8df8
Hikikomori <91256870+developer-ongar@users.noreply.github.com>
2022-06-08 12:43:38 +0600

    Update ARButton.js

--- ARButton.js
+++ ARButton.js
@@ -132,3 +132,20 @@ class ARButton {
 			button.textContent = 'AR NOT SUPPORTED';
 
 		}
+		
+		function stylizeElement( element ) {
+
+			element.style.position = 'absolute';
+			element.style.bottom = '20px';
+			element.style.padding = '12px 6px';
+			element.style.border = '1px solid #fff';
+			element.style.borderRadius = '4px';
+			element.style.background = 'rgba(0,0,0,0.1)';
+			element.style.color = '#fff';
+			element.style.font = 'normal 13px sans-serif';
+			element.style.textAlign = 'center';
+			element.style.opacity = '0.5';
+			element.style.outline = 'none';
+			element.style.zIndex = '999';
+
+		}
