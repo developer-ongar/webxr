8e87c2e6d51d670f37a07cb322f92d267c64bc01
Hikikomori <91256870+developer-ongar@users.noreply.github.com>
2022-06-09 15:28:22 +0600

    Update OculusHandModel.js

--- OculusHandModel.js
+++ OculusHandModel.js
@@ -1,2 +1,31 @@
 import { Object3D, Sphere, Box3 } from '../libs/three.module.js';
 import { XRHandMeshModel } from './XRHandMeshModel.js';
+
+const TOUCH_RADIUS = 0.01;
+const POINTING_JOINT = 'index-finger-tip';
+
+class OculusHandModel extends Object3D {
+
+	constructor( controller ) {
+
+		super();
+
+		this.controller = controller;
+		this.motionController = null;
+		this.envMap = null;
+
+		this.mesh = null;
+
+		controller.addEventListener( 'connected', ( event ) => {
+
+			const xrInputSource = event.data;
+
+			if ( xrInputSource.hand && ! this.motionController ) {
+
+				this.xrInputSource = xrInputSource;
+
+				this.motionController = new XRHandMeshModel( this, controller, this.path, xrInputSource.handedness );
+
+			}
+
+		} );
